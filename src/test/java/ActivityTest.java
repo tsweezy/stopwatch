b1a089545c1f20fd960ba1231f703a2c1da5f5a3
tsweezy <sweettre216@gmail.com>
2020-12-08 11:12:26 -0500

    Add tests for Activity.java

---
+++ src/test/java/ActivityTest.java
@@ -0,0 +1,37 @@
+import org.junit.jupiter.api.*;
+
+import java.time.Instant;
+
+class ActivityTest {
+    Activity a;
+
+    @Test
+    void constructorEmpty_1() {
+        a = new Activity();
+        Assertions.assertNull(a.getName());
+    }
+
+    @Test
+    void constructorEmpty_2() {
+        a = new Activity();
+        Assertions.assertNull(a.getDescription());
+    }
+
+    @Test
+    void constructorOneParam() {
+        a = new Activity("Activity Name");
+        Assertions.assertEquals(a.getName(), "Activity Name");
+    }
+
+    @Test
+    void constructorTwoParams() {
+        a = new Activity("Name", "Description");
+        Assertions.assertEquals(a.getDescription(), "Description");
+    }
+
+    @Test
+    void start_1() {
+        a.start();
+        Assertions.assertEquals(a.getStart(), Instant.now());
+    }
+}
